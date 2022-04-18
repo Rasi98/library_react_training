81d3c81f9d48c348361bcbfd976f6878bbbb6a17
Semini <2019cs158@stu.ucsc.lk>
2022-04-18 15:31:18 +0530

    Welcome section done

---
+++ src/components/Library.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Container, Row, Col} from "react-bootstrap";
+import Welcome from "./Welcome";
+
+const Library: React.FC = () => {
+    return(
+    <Container fluid={true}>
+        <Row>
+            <Col xs={12}>
+                <Welcome />
+            </Col>
+        </Row>
+    </Container>
+    )
+}
+
+export default Library;

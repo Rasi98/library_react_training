dc623f1e37e449da276456691f9e832ac7d2308e
Semini <2019cs158@stu.ucsc.lk>
2022-04-23 11:05:06 +0530

    book section ui

--- src/components/Library.tsx
+++ src/components/Library.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {Col, Container, Row} from "react-bootstrap";
 import Welcome from "./Welcome";
+import Books from "./books/Books";
 
 const Library: React.FC = () => {
     return (
@@ -9,6 +10,12 @@ const Library: React.FC = () => {
         <Col xs={12}>
             <Welcome />
         </Col>
+    </Row>
+    <Row>
+        <Col xs={6} className='ps-5'>
+            <Books />
+        </Col>
+        <Col xs={6} className='ps-5'>Authors</Col>
     </Row>
 </Container>
     )
---
+++ src/components/books/BookTitle.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {Col, Row} from "react-bootstrap";
+
+const BookTitle:React.FC = () => {
+    return (
+            <Col xs={12}>
+                <h2 className='mt-2'>Books</h2>
+            </Col>
+    )
+}
+
+export default BookTitle;
